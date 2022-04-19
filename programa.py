ed3508beac69d5389f6500e4dadc4a4e611d8ed8
Lu15Gustavo <102445868+Lu15Gustavo@users.noreply.github.com>
2022-04-19 20:53:16 -0300

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,20 @@
+
+nome = input("Insira seu Nome: ")
+
+data = input("Insira sua data de nascimento: ")
+
+txt = nome
+zebra = "nasceu no dia "
+
+PARTE1data = data[0:2]
+PARTE2data = data[2:4]
+PARTE3data = data[4:8]
+
+dataFormatado = PARTE1data +"/"+ PARTE2data +"/"+ PARTE3data
+
+print( txt + zebra + dataFormatado )
+
+
+
+
+#Tranquilo essa 
